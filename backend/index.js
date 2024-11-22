970e14bbfe33e57a44d705571060abe0281cd51d
mininha <a0103656953@gmail.com>
2024-11-22 15:14:51 +0900

    서버측 코드 추가 및 변경

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const path = require('path')
+//노드 설치시 기본적으로 같이 다운됨
+
 const app = express()
 const port = 3000
-
+app.use(express.static('public'))
+//퍼블릭 개방된
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  // res.send('<h1>Hello World!</h1>')
+  res.sendFile(path.join(__dirname, "notice.html"))
 })
 
 app.listen(port, () => {
